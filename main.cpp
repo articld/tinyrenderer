541e9a716f2af403be8d864a63b5f7b0e3e138d9
Enrico Mazzon <enricomazzon@live.it>
2026-02-01 15:43:27 +0100

    rendering linea

--- main.cpp
+++ main.cpp
@@ -7,6 +7,15 @@ constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
+void draw_line_2d(const int ax, const int ay, const int bx, const int by, TGAImage &framebuffer, TGAColor color){
+    for(float t=0; t <= 1; t+=0.02){
+        int x = std::round(ax + t*(bx-ax));
+        int y = std::round(ay + t*(by-ay));
+        framebuffer.set(x, y, color);
+    }
+
+}
+
 int main(int argc, char** argv) {
     constexpr int width  = 64;
     constexpr int height = 64;
@@ -15,6 +24,11 @@ int main(int argc, char** argv) {
     int ax =  7, ay =  3;
     int bx = 12, by = 37;
     int cx = 62, cy = 53;
+
+    draw_line_2d(ax, ay, bx, by, framebuffer, yellow);
+    draw_line_2d(bx, by, cx, cy, framebuffer, blue);
+    draw_line_2d(cx, cy, ax, ay, framebuffer, green);
+    draw_line_2d(ax, ay, cx, cy, framebuffer, red);
 
     framebuffer.set(ax, ay, white);
     framebuffer.set(bx, by, white);
