89499199d8061231d68fec19202af6c95984759a
Enrico Mazzon <enricomazzon@live.it>
2026-02-07 21:48:15 +0100

    Trinagolo rgb

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ double signed_triangle_area(int ax, int ay, int bx, int by, int cx, int cy){
     return .5*((by-ay)*(bx+ax) + (cy-by)*(cx+bx) + (ay-cy)*(ax+cx));
 }
 
-void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color) {
+void triangle(int ax, int ay, int az, int bx, int by, int bz, int cx, int cy, int cz, TGAImage &framebuffer, TGAColor color) {
     int bbminx = std::min(std::min(ax,bx), cx);
     int bbminy = std::min(std::min(ay,by), cy);
     int bbmaxx = std::max(std::max(ax,bx), cx);
@@ -62,16 +62,24 @@ void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuf
             double beta = signed_triangle_area(x, y, cx, cy, ax, ay) / total_area;
             double gamma = signed_triangle_area(x, y, ax, ay, bx, by) / total_area;
             if (alpha <0 || beta <0 || gamma <0) continue;
-            framebuffer.set(x,y, color);
+            unsigned char b = 0, r = 0, g = 0;
+            if( alpha > 0.1)
+                b = static_cast<unsigned char>(alpha * az);
+            if( beta > 0.1)
+                r = static_cast<unsigned char>(beta * bz);
+            if( gamma > 0.1)
+                g = static_cast<unsigned char>(gamma * cz);
+            framebuffer.set(x,y, {b, r, g});
         }
     }
 }
 
 int main(int argc, char** argv) {
     TGAImage framebuffer(width, height, TGAImage::RGB);
-    triangle(  7, 45, 35, 100, 45,  60, framebuffer, red);
-    triangle(120, 35, 90,   5, 45, 110, framebuffer, white);
-    triangle(115, 83, 80,  90, 85, 120, framebuffer, green);
+    int ax = 17, ay = 4, az = 255;
+    int bx = 120, by = 110, bz= 255;
+    int cx = 23, cy = 90, cz = 255;
+    triangle(ax, ay, az, bx, by, bz, cx, cy, cz, framebuffer, green);
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
 }
