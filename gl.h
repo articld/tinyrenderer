cbc6414dc04b4ea1e17b5727f8a307f86a335896
Enrico Mazzon <enricomazzon@live.it>
2026-03-06 21:25:44 +0100

    Non avevo tracciato i file gl. *facepalm*

---
+++ gl.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "tgaimage.h"
+#include "vec.h"
+
+#include <algorithm>
+#include <iostream>
+
+void lookat(const vec3 eye, const vec3 center, const vec3 up);
+void init_perspective(const double f);
+void init_viewport(const int x, const int y, const int w, const int h);
+void init_zbuffer(const int width, const int height);
+void init_lightmap(const int width, const int height);
+
+struct IShader {
+    virtual std::pair<bool,TGAColor> fragment(const vec3 bar) const = 0;
+};
+
+typedef vec4 Triangle[3];
+void rasterize(const Triangle &clip, const IShader &shader, TGAImage &framebuffer);
+void shadowmap(const Triangle &clip, TGAImage& lightbuffer);
